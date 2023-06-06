ee4edf9196d4969f9c1d24c4b2b2e64bdad12cfa
warsena <dnaawarsena@gmail.com>
2023-06-06 21:12:12 +0800

    add pic to update

--- module3/update3.php
+++ module3/update3.php
@@ -3,6 +3,7 @@
 
 <head>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <img src="FKEduSearch/img.png" style="height:200px" width="1520px">
     <title>FK_EDUSEARCH</title>
 </head>
 
