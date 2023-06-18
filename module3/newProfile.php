173819817ac3fa8660d3119a250695dcf40198ae
warsena <dnaawarsena@gmail.com>
2023-06-19 05:22:56 +0800

    create new

--- module3/newProfile.php
+++ module3/newProfile.php
@@ -1,3 +1,61 @@
+<?php
+// Connect to the database server.
+$link = mysqli_connect("localhost", "root", "warsena180102_", "fkedusearch") or die(mysqli_connect_error());
+
+// Create the expert table if it doesn't exist
+$createTableQuery = "CREATE TABLE IF NOT EXISTS expert (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    inputName VARCHAR(50),
+    inputEmail VARCHAR(50),
+    inputPhone VARCHAR(15),
+    inputResearch VARCHAR(100),
+    inputList VARCHAR(100),
+    inputAcademic VARCHAR(50),
+    inputCV VARCHAR(100),
+    inputSocialMedia VARCHAR(100)
+)";
+mysqli_query($link, $createTableQuery) or die(mysqli_error($link));
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form data
+    $name = $_POST["inputName"];
+    $email = $_POST["inputEmail"];
+    $phoneNum = $_POST["inputPhone"];
+    $researchArea = $_POST["inputResearch"];
+    $listOfPublications = $_POST["inputList"];
+    $academicStatus = $_POST["inputAcademic"];
+    $cv = $_FILES["inputCV"]["name"];
+    $socialMedia = $_POST["inputSocialMedia"];
+
+    // Validate form fields
+    if (empty($name) || empty($email) || empty($phoneNum) || empty($researchArea) || empty($listOfPublications) || empty($academicStatus) || empty($cv) || empty($socialMedia)) {
+        echo '<script>alert("Please fill in all the fields!!");</script>';
+    } else {
+        // Construct INSERT query
+        $insertQuery = "INSERT INTO expert (inputName, inputEmail, inputPhone, inputResearch, inputList, inputAcademic, inputCV, inputSocialMedia) 
+        VALUES ('$name', '$email', '$phoneNum', '$researchArea', '$listOfPublications', '$academicStatus', '$cv', '$socialMedia')";
+
+        // Execute INSERT query
+        if (mysqli_query($link, $insertQuery)) {
+            echo "Record inserted successfully.";
+            echo '<script>window.location.href = "view.php";</script>'; // Redirect to view.php
+            exit();
+        } else {
+            echo "Error inserting record: " . mysqli_error($link);
+        }
+    }
+}
+
+$name = "";
+$email = "";
+$phoneNum = "";
+$researchArea = "";
+$listOfPublications = "";
+$academicStatus = "";
+$cv = "";
+$socialMedia = "";
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -8,101 +66,17 @@
 </head>
 
 <body>
-
-    <?php include '../UserSideBar/User_sidebar.php' ?>
-
-    <?php
-    // Connect to the database server.
-    $link = mysqli_connect("localhost", "root", "warsena180102_") or die(mysqli_connect_error());
-
-    // Create the database if it doesn't exist
-    $createDBQuery = "CREATE DATABASE IF NOT EXISTS fkedusearch";
-    mysqli_query($link, $createDBQuery) or die(mysqli_error($link));
-
-
-    // Select the database
-    mysqli_select_db($link, "fkedusearch") or die(mysqli_error($link));
-
-    // Create the expert table if it doesn't exist
-    $createTableQuery = "CREATE TABLE IF NOT EXISTS expert (
-    id INT(11) PRIMARY KEY,
-    inputName VARCHAR(50),
-    inputEmail VARCHAR(50),
-    inputPhone VARCHAR(15),
-    inputResearch VARCHAR(100),
-    inputList VARCHAR(100),
-    inputAcademic VARCHAR(50),
-    inputCV VARCHAR(100),
-    inputSocialMedia VARCHAR(100)
-)";
-    mysqli_query($link, $createTableQuery) or die(mysqli_error($link));
-
-
-
-
-
-
-
-
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-        // Retrieve form data
-        $id = $_POST["id"];
-        $name = $_POST["inputName"];
-        $email = $_POST["inputEmail"];
-        $phoneNum = $_POST["inputPhone"];
-        $researchArea = $_POST["inputResearch"];
-        $listOfPublications = $_POST["inputList"];
-        $academicStatus = $_POST["inputAcademic"];
-        $cv = $_FILES["inputCV"]["name"];
-        $socialMedia = $_POST["inputSocialMedia"];
-
-        // Validate form fields
-        if (empty($id) || empty($Name) || empty($email) || empty($phoneNum) || empty($researchArea) || empty($listOfPublications) || empty($academicStatus) || empty($cv) || empty($socialMedia)) {
-            echo '<script>alert("Please fill in all the fields!!");</script>';
-        } else {
-            // Construct INSERT query
-            $insertQuery = "INSERT INTO expert (id, inputName, inputEmail, inputPhone, inputResearch, inputList, inputAcademic, inputCV, inputSocialMedia) 
-            VALUES ('$id', '$name', '$email', '$phoneNum', '$researchArea', '$listOfPublications', '$academicStatus', '$cv', '$socialMedia')";
-
-            // Execute INSERT query
-            if (mysqli_query($link, $insertQuery)) {
-                echo "Record inserted successfully.";
-                header("Location: view.php"); // Redirect to view.php
-                exit();
-            } else {
-                echo "Error inserting record: " . mysqli_error($link);
-            }
-        }
-    }
-
-    // // Fetch the categories from the database
-    // $expertQuery = "SELECT * FROM expert";
-    // $expertResult = mysqli_query($link, $categoryQuery);
-
-    // Initialize variables with empty values
-    $id = "";
-    $name = "";
-    $emai = "";
-    $phoneNum = "";
-    $researchArea = "";
-    $listOfPublications = "";
-    $academicStatus = "";
-    $cv = "";
-    $socialMedia = "";
-    ?>
-
-
-
+    <?php include '../UserSideBar/User_sidebar.php'?>
 
     <div class="card-body">
-        <form method="POST" action="createProfileNew.php">
+        <form method="POST" action="<?php echo $_SERVER['PHP_SELF']; ?>" enctype="multipart/form-data">
             <div class="col-md-4">
                 <div class="text-center">
                     <img alt="avatar" src="/FKEduSearch/FK_edusearch/module3/avatar.png" class="rounded-circle img-responsive mt-2" width="128" height="128" />
                     <div class="mt-2">
                         <label for="imageUpload" class="btn btn-primary">
                             <i class="fas fa-upload"></i> Upload
-                            <input type="file" id="imageUpload" style="display: none;">
+                            <input type="file" id="imageUpload" name="imageUpload" style="display: none;">
                         </label>
                     </div>
 
@@ -119,9 +93,7 @@
                         });
                     </script>
 
-
-                    <small>For best results, use an image at least 128px by 128px in .jpg
-                        format</small>
+                    <small>For best results, use an image at least 128px by 128px in .jpg format</small>
                 </div>
             </div>
 
@@ -146,7 +118,6 @@
                 </div>
             </div>
 
-
             <div class="row">
                 <div class="mb-3 col-md-6">
                     <label for="inputResearch">Research Area</label>
@@ -170,7 +141,7 @@
 
             <div class="row">
                 <div class="mb-3 col-md-6">
-                    <label for="title" class="col-sm-2 col-form-label">CV</label>
+                    <label for="inputCV">CV</label>
                     <input type="file" class="form-control" id="inputCV" name="inputCV" placeholder="Upload CV">
                 </div>
             </div>
@@ -180,22 +151,22 @@
                     <label for="inputSocialMedia">SOCIAL MEDIA</label>
                     <div class="mb-2">
                         <i class="align-middle me-2 fab fa-fw fa-facebook"></i>
-                        <input type="fb" class="form-control" id="inputSocialMedia" name="inputSocialMedia" placeholder="">
+                        <input type="text" class="form-control" id="inputSocialMedia" name="inputSocialMediaFacebook" placeholder="Facebook">
                     </div>
 
                     <div class="mb-2">
                         <i class="align-middle me-2 fab fa-fw fa-twitter"></i>
-                        <input type="twitter" class="form-control" id="inputSocialMedia" name="inputSocialMedia" placeholder="">
+                        <input type="text" class="form-control" id="inputSocialMedia" name="inputSocialMediaTwitter" placeholder="Twitter">
                     </div>
 
                     <div class="mb-2">
                         <i class="align-middle me-2 fab fa-fw fa-instagram"></i>
-                        <input type="ig" class="form-control" id="inputSocialMedia" name="inputSocialMedia" placeholder="">
+                        <input type="text" class="form-control" id="inputSocialMedia" name="inputSocialMediaInstagram" placeholder="Instagram">
                     </div>
 
                     <div class="mb-2">
                         <i class="align-middle me-2 fab fa-fw fa-linkedin"></i>
-                        <input type="link" class="form-control" id="inputSocialMedia" name="inputSocialMedia" placeholder="">
+                        <input type="text" class="form-control" id="inputSocialMedia" name="inputSocialMediaLinkedIn" placeholder="LinkedIn">
                     </div>
                 </div>
             </div>
@@ -203,17 +174,7 @@
             <button type="submit" class="btn btn-primary">SAVE</button>
             <button type="submit" class="btn btn-primary">EDIT</button>
         </form>
-
     </div>
-
-
-
-
-
-
-
-
-
 
 </body>
 
